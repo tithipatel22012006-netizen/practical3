f41463a1789fe26606fffafe00eb917e2f5a99f0
tithipatel22012006-notizen <tithipatel22012006@gmail.com>
2026-08-15 21:44:32 +0530

    Add App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,28 @@
 import React from "react"; 
-import Student from "./Student"; 
- 
-function App() {  
-   return (     
-   <div>     
-      <h1>Student Details</h1>  
-
-            <Student      
-               name="Rahul Kumar"        
-                rollNo="101"         
-                course="BCA"        
-                 marks="85%"       
-                 />       
-
-                  <Student        
-                   name="Priya Singh"        
-                    rollNo="102"         
-                    course="BCA"         
-                    marks="92%"      
-                  />       
-                      
-                      <Student         
-                      name="Amit Sharma"         
-                      rollNo="103"        
-                       course="BCA"         
-                       marks="78%"      
-                        />     
-                        </div>   
-                        ); 
-                      }  
-                      export default App;
+import Student from "./Student";
+  function App() {  
+    return (     
+    <div>      
+       <h1>Student Details</h1> 
+              <Student         
+              name="Rahul Kumar"        
+               rollNo="101"         
+               course="BCA"         
+               marks="85%"      
+                />        
+                <Student         
+                name="Priya Singh"         
+                rollNo="102"         
+                course="BCA"         
+                marks="92%"      
+                 />        
+                 <Student        
+                  name="Amit Sharma"        
+                   rollNo="103"         
+                   course="BCA"         
+                   marks="78%"       
+                   />    
+                    </div>  
+                     ); 
+                  } 
+ export default App; 
